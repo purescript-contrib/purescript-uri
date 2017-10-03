7a770c9b0c76416f52e23d60d1ba3cdcd359302e
Gary Burgess <gary.burgess@gmail.com>
2017-10-03 15:17:54 +0100

    Fix behaviour of semicolons in query parts
    We were decoding percent encoded aspects of the query string before
    doing the splitting on `&` and `;` delimiters, meaning it was
    impossible to determine where a split actually was, as we lost the
    information about which characters had been encoded or not.

--- src/Data/URI/Fragment.purs
+++ src/Data/URI/Fragment.purs
@@ -2,13 +2,31 @@ module Data.URI.Fragment (parser, print) where
 
 import Prelude
 
+import Control.Alt ((<|>))
+import Data.Either (fromRight)
+import Data.String as S
+import Data.String.Regex as RX
+import Data.String.Regex.Flags as RXF
 import Data.URI (Fragment(..))
-import Data.URI.Common (joinWith, parseFragmentOrQuery, printFragmentOrQuery)
+import Data.URI.Common (decodePCTComponent, joinWith, parsePChar)
+import Global (encodeURIComponent)
+import Partial.Unsafe (unsafePartial)
 import Text.Parsing.StringParser (Parser)
 import Text.Parsing.StringParser.Combinators (many)
+import Text.Parsing.StringParser.String (string)
 
 parser ∷ Parser Fragment
-parser = Fragment <<< joinWith "" <$> many parseFragmentOrQuery
+parser = Fragment <<< joinWith ""
+  <$> many (parsePChar decodePCTComponent <|> string "/" <|> string "?")
 
 print ∷ Fragment → String
-print (Fragment f) = printFragmentOrQuery f
+print (Fragment f) = S.joinWith "" $ map printChar $ S.split (S.Pattern "") f
+  where
+  -- Fragments & queries have a bunch of characters that don't need escaping
+  printChar ∷ String → String
+  printChar s
+    | RX.test rxPrintable s = s
+    | otherwise = encodeURIComponent s
+
+rxPrintable ∷ RX.Regex
+rxPrintable = unsafePartial fromRight $ RX.regex "[&;$+=/?:@]" RXF.global
