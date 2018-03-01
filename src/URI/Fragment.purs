b11ee9a372d733b364bd5eb70836d6c4309acfde
Gary Burgess <gary.burgess@gmail.com>
2018-03-01 14:54:22 +0000

    Add fragment doc comments

--- src/URI/Fragment.purs
+++ src/URI/Fragment.purs
@@ -19,7 +19,7 @@ import Text.Parsing.Parser (Parser)
 import Text.Parsing.Parser.String (char)
 import URI.Common (parseSubDelims, parseUnreserved, pctEncoded, printEncoded)
 
--- | The hash fragment of a URI.
+-- | The fragment component (hash) of a URI.
 newtype Fragment = Fragment String
 
 derive newtype instance eqFragment ∷ Eq Fragment
@@ -30,24 +30,52 @@ derive newtype instance monoidFragment ∷ Monoid Fragment
 instance showFragment ∷ Show Fragment where
   show (Fragment s) = "(Fragment.unsafeFromString " <> show s <> ")"
 
+-- | Constructs a fragment value from a string, percent-encoding any characters
+-- | that require it. Note that running this on a string that has already had
+-- | percent-encoding applied will double-encode it, for those situations use
+-- | `unsafeFromString` instead.
+-- |
+-- | ``` purescript
+-- | fromString "foo" = unsafeFromString "foo"
+-- | fromString "foo#bar" = unsafeFromString "foo%23bar"
+-- | fromString "foo%23bar" = unsafeFromString "foo%2523bar"
+-- | ```
 fromString ∷ String → Fragment
 fromString = Fragment <<< printEncoded fragmentChar
 
+-- | Returns the string value for a fragment, percent-decoding any characters
+-- | that require it.
+-- |
+-- | ``` purescript
+-- | toString (unsafeFromString "foo") = "foo"
+-- | toString (unsafeFromString "foo%23bar") = "foo#bar"
+-- | ```
 toString ∷ Fragment → String
 toString (Fragment s) = decodeURIComponent s
 
+-- | Constructs a fragment value from a string directly - no percent-encoding
+-- | will be applied. This is useful when using a custom encoding scheme for
+-- | the fragment, to prevent double-encoding.
 unsafeFromString ∷ String → Fragment
 unsafeFromString = Fragment
 
+-- | Returns the string value for the fragment without percent-decoding. Only
+-- | "unsafe" in the sense that values this produces may need further decoding,
+-- | the name is more for symmetry with the `fromString`/`unsafeFromString`
+-- | pairing.
 unsafeToString ∷ Fragment → String
 unsafeToString (Fragment s) = s
 
+-- | A parser for the fragment component of a URI. Expects values with a `'#'`
+-- | prefix.
 parser ∷ Parser String Fragment
 parser =
   char '#' *>
     (Fragment <<< String.joinWith ""
       <$> Array.many (pctEncoded <|> String.singleton <$> fragmentChar))
 
+-- | A printer for the fragment component of the URI. Will print the value with
+-- | a `'#'` prefix.
 print ∷ Fragment → String
 print (Fragment f) = "#" <> f
 
