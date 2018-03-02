78119e544708825766e55c251a2cfb109f0dc2d1
Gary Burgess <gary.burgess@gmail.com>
2018-03-02 12:44:18 +0000

    Doc comments for HostPortPair

--- src/URI/HostPortPair.purs
+++ src/URI/HostPortPair.purs
@@ -13,8 +13,23 @@ import URI.Host as Host
 import URI.Port (Port)
 import URI.Port as Port
 
+-- | A spec-conformant host/port pair (may also be empty).
+-- | For example: `purescript.org`, `localhost:3000`, `:9000`.
 type HostPortPair host port = Maybe (These host port)
 
+-- | A parser for a spec-conformant host/port pair.
+-- |
+-- | Additionally, this function allows for the `Host` and `Port` components to
+-- | be parsed into custom representations. If this is not necessary, use `pure`
+-- | for both these arguments.
+-- |
+-- | Host parsing is dealt with a little differently to all the other URI
+-- | components, as for hosts the control is passed entirely to the component
+-- | parser. This is to accomodate multi-host URIs that are used sometimes for
+-- | connection strings and the like, but as these are not spec-conforming this
+-- | part of parsing may need to bend the rules a little. See
+-- | [`URI.Extra.MultiHostPortPair`](../URI.Extra.MultiHostPortPair) for an
+-- | example of this.
 parser
   ∷ ∀ host port
   . (Host → Either URIPartParseError host)
@@ -29,6 +44,11 @@ parser parseHost parsePort = do
     Just h, Just p → Just (Both h p)
     Nothing, Nothing → Nothing
 
+-- | A printer for a spec-conformant host/port pair.
+-- |
+-- | As a counterpart to the `parser` this function also requires the `Host`
+-- | and `Port` components to be printed back from their custom representations.
+-- | If no custom types are being used, pass `id` for both of these arguments.
 print
   ∷ ∀ host port
   . (host → Host)
