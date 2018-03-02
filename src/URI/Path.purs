f868610ef38e18786ec52d623aadc3325cad7684
Gary Burgess <gary.burgess@gmail.com>
2018-03-02 14:15:09 +0000

    Path doc comments (and added TODO for NEA note)

--- src/URI/Path.purs
+++ src/URI/Path.purs
@@ -11,7 +11,11 @@ import Text.Parsing.Parser (Parser)
 import Text.Parsing.Parser.String (char)
 import URI.Path.Segment (PathSegment, parseSegment, unsafeSegmentToString)
 
-newtype Path = Path (Array PathSegment)
+-- | A generic absolute path. Used in both hierarchical-part and relative-parts
+-- | when an authority component is present. Corresponds to path-abempty in
+-- | the spec (although the empty part is tracked in a `Maybe` in
+-- | `HierarchicalPart` or `RelativePart` in this library).
+newtype Path = Path (Array PathSegment) -- TODO: this should use `NonEmptyArray`
 
 derive newtype instance eqPath ∷ Eq Path
 derive newtype instance ordPath ∷ Ord Path
@@ -20,8 +24,10 @@ derive newtype instance monoidPath ∷ Monoid Path
 derive instance genericPath ∷ Generic Path _
 instance showPath ∷ Show Path where show = genericShow
 
+-- | A parser for a generic absolute path URI component.
 parser ∷ Parser String Path
 parser = Path <$> Array.some (char '/' *> parseSegment)
 
+-- | A printer for a generic absolute path URI component.
 print ∷ Path → String
 print (Path segs) = "/" <> String.joinWith "/" (map unsafeSegmentToString segs)
